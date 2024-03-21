3c4955847e93220235e65fb0b419fd5d8951900d
Danniel Navas <dannielnavas@gmail.com>
2024-03-20 21:43:45 -0500

    ✨ Agrega archivos y configuraciones iniciales para Astro.

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://danniel.dev/",
+  integrations: [tailwind(), sitemap(), robotsTxt()],
+});
