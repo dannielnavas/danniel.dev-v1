6151c555d1a4153a6b801ec87ce73627fa576cb6
Danniel Navas <dannielnavas@gmail.com>
2024-03-31 20:07:15 -0500

    ✨ Agrega configuración de relación de aspecto a tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      aspectRatio: {
+        "16/9": [16, 9],
+        "4/3": [4, 3],
+        "1/1": [1, 1],
+        "3/2": [3, 2],
+      },
+    },
+  },
+  plugins: [],
+};
