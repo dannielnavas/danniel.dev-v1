ce64d8449b91c2d877afe7520020f157b8aa542a
Danniel Navas <dannielnavas@gmail.com>
2024-04-03 21:47:16 -0500

    ✨ Agregada función para obtener proyectos de Github

---
+++ src/functions/projets.ts
@@ -0,0 +1,9 @@
+import type { IProject } from "@/shared/models/project.interface";
+
+export async function getProjectsGithub(): Promise<IProject[]> {
+  const url = "https://api.github.com/users/DannielNavas/repos?per_page=100";
+  const response = await fetch(url);
+  const projects = await response.json();
+  console.log(projects);
+  return projects;
+}
