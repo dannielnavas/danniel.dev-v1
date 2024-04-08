464a9eae99857024c20c833678f5786d6faf4532
Danniel Navas <dannielnavas@gmail.com>
2024-04-07 19:43:22 -0500

    ✨ Añadir función getNumbers para obtener números desde el backend

---
+++ src/functions/numbers.ts
@@ -0,0 +1,8 @@
+import type { INumbers } from "@/shared/models/numbers.interface";
+
+export async function getNumbers(): Promise<INumbers[]> {
+  const url = "https://backend-danniel-dev.vercel.app/statistic";
+  const response = await fetch(url);
+  const numbers = await response.json();
+  return numbers;
+}
