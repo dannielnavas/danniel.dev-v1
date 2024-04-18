8e536534c52f040eb625d73783b6cc35fd0dccee
Danniel Navas <dannielnavas@gmail.com>
2024-04-17 21:07:15 -0500

    ✨ Añadir archivo de constantes para tecnologías y modelo de interfaz de tecnología

---
+++ src/shared/const/technologies.const.ts
@@ -0,0 +1,32 @@
+import type { Technology } from "../models/technology.interface";
+
+export const TECHNOLOGIES: Technology[] = [
+  {
+    name: "Angular",
+    icon: "/img/technologies/angular.webp",
+  },
+  {
+    name: "JavaScript",
+    icon: "/img/technologies/javascript.webp",
+  },
+  {
+    name: "TypeScript",
+    icon: "/img/technologies/typescript.webp",
+  },
+  {
+    name: "node.js",
+    icon: "/img/technologies/nodejs.webp",
+  },
+  {
+    name: "NestJS",
+    icon: "/img/technologies/nestjs.webp",
+  },
+  {
+    name: "AWS",
+    icon: "/img/technologies/aws.webp",
+  },
+  {
+    name: "Firebase",
+    icon: "/img/technologies/firebase.webp",
+  },
+];
