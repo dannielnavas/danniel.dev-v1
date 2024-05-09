50cea84ba9e84a62205a8cb40d66ee8c2900035b
Danniel Navas <dannielnavas@gmail.com>
2024-05-08 21:12:27 -0500

    ✨ feat: Agregar soporte para @astrojs/vercel en astro.config.mjs y package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
