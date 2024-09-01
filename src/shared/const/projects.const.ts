977012d999b1526c2790ad87f9823cb8a111225c
Danniel Navas <dannielnavas@gmail.com>
2024-09-01 16:40:39 -0500

    💄  feat: change section projects

---
+++ src/shared/const/projects.const.ts
@@ -0,0 +1,12 @@
+import type { IProject } from "../models/project.interface";
+
+export const PROJECTS: IProject[] = [
+  {
+    name: "DevtoSpanish",
+    description: "A Chrome extension that translates DEV articles to Spanish.",
+    technologies: ["VueJs", "TypeScript", "Tailwind CSS"],
+    github: "",
+    live: "https://devtospanish.danniel.dev",
+    image: "/img/projects/devtospanish.jpeg",
+  },
+];
