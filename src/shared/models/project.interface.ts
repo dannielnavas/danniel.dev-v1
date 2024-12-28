01f973e21d12821f42283fd5be24a21d10bca2f9
Danniel Navas <dannielnavas@gmail.com>
2024-12-28 16:32:35 -0500

    ✨ feat: Actualizar la interfaz IProject para incluir detalles de tecnologías con nombre, clase e ícono

--- src/shared/models/project.interface.ts
+++ src/shared/models/project.interface.ts
@@ -1,7 +1,11 @@
 export interface IProject {
   name: string;
   description: string;
-  technologies: string[];
+  technologies: {
+    name: string;
+    class: string;
+    icon: (_props: Record<string, any>) => any;
+  }[];
   github: string;
   live: string;
   image: string;
